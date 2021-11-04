deacd9bbe52964057d96dff537a240257fb80fad
Doug Prostko <dougtko@gmail.com>
2021-11-04 14:58:20 -0400

    working on octave

--- src/app/octave/octave.component.ts
+++ src/app/octave/octave.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'octave',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./octave.component.scss']
 })
 export class OctaveComponent implements OnInit {
+  @Input() value!: string;
+  @Output() newValueEvent = new EventEmitter<any>();
+
+  name!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.name = "octave";
+  }
+
+  changeOctave(event: any) {
+    this.value = event.args.value;
+    this.newValueEvent.emit({field: this.name, value: this.value});
   }
 
 }
