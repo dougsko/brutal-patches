924ed9c7f73cc1f7569ecd02e58c8bc5b5ae084e
Doug Prostko <dougtko@gmail.com>
2021-11-04 13:25:36 -0400

    sliders are working

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-slider',
+  selector: 'slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
+  @Input() name!: string;
+  @Input() min!: string;
+  @Input() max!: string;
+  @Input() value!: string;
+  @Input() lower!: string;
+  @Output() newValueEvent = new EventEmitter<any>();
+
+  label!: string;
+  value_data!: number;
+  min_data!: number;
+  max_data!: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    // this.label = this.name.charAt(0).toUpperCase() + this.name.slice(1);
+    this.label = this.formatName(this.name);
+
+    if (this.value) {
+      this.value_data = parseInt(this.value);
+    }
+
+    if(this.min) {
+      this.min_data = parseInt(this.min);
+    }
+  
+    if(this.max) {
+      this.max_data = parseInt(this.max);
+    }
+
+  }
+
+  formatName(name: string): string {
+    if(name === "vca") {
+      return name.toUpperCase();
+    }
+    let words: string[] = [];
+    for(let word of name.split("_")) {
+      words.push(word.charAt(0).toUpperCase() + word.slice(1));
+    }
+    return words.join(" ");
+  }
+
+  moveSlider(event: any) {
+    this.value = event.args.value;
+    this.newValueEvent.emit({field: this.name, value: this.value});
   }
 
 }
