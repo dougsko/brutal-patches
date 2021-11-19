54040fd70a8a5005ef89a93af8d0caac74ecea25
Doug Prostko <dougtko@gmail.com>
2021-11-18 19:51:53 -0500

    started server-side

--- server-src/src/app.module.ts
+++ server-src/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatchController } from './patch/patch.controller';
 import { PatchService } from './patch/patch.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, PatchController],
-  providers: [AppService, PatchService]
+  controllers: [AppController, PatchController, AuthController],
+  providers: [AppService, PatchService, AuthService]
 })
 export class AppModule {}
