17580f9cb33640b931ae2b63b712b2468ccf6bb1
Doug Prostko <dougtko@gmail.com>
2021-12-10 11:08:48 -0500

    ...

--- server-src/src/main.ts
+++ server-src/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   );
   await app.register(fastifyHelmet);
   app.enableCors({
-    origin: '*', 
+    origin: 'https://brutalpatches.com', 
  });
   await app.listen(port);
   Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
